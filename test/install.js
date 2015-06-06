bd4df52ce102ba2ca093af08d0b182315b6b16d4
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2015-06-05 20:29:36 -0700

    added testing for installing a package

---
+++ test/install.js
@@ -0,0 +1,25 @@
+'use strict';
+
+let bluebird = require('bluebird');
+let exec = bluebird.promisify(require('child_process').exec);
+let expect = require('chai').expect;
+
+process.env.PORT = '7129';
+require('../server');
+let registry = 'http://localhost:' + process.env.PORT;
+
+describe('install', function () {
+  it('can install heroku-git', function () {
+    return exec('npm uninstall heroku-git')
+    .then(function () {
+      return exec(`npm install heroku-git --registry ${registry}`);
+    })
+    .then(function (output) {
+      let stdout = output[0];
+      expect(stdout).to.match(/^heroku-git/);
+    })
+    .finally(function () {
+      exec('npm uninstall heroku-git');
+    });
+  });
+});
