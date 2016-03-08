71e3fe8b846fc51bdea520d7bd55b532c3e4be0d
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2016-03-08 14:49:37 -0800

    structured logging

---
+++ logger.js
@@ -0,0 +1,10 @@
+'use strict';
+
+let metric = require('metric-log');
+
+module.exports = function* (next) {
+  this.metric = metric.context({request_id: this.request.headers['x-request-id'] || Math.random()});
+  let end = this.metric.profile('request');
+  yield next;
+  end({method: this.request.method, 'user-agent': this.request.headers['user-agent'], status: this.response.status, path: this.request.path});
+};
