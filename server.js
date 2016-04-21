a5e63fa9ad24d8bebb5689f5e043a6f0d15cfc26
Jeff Dickey <dickeyxxx@gmail.com>
2016-04-20 19:48:40 -0700

    standardify

--- server.js
+++ server.js
@@ -1,177 +1,175 @@
-'use strict';
+'use strict'
 
-if (!process.env.NEW_RELIC_LICENSE_KEY) process.env.NEW_RELIC_ENABLED = 'false';
-process.env.NEW_RELIC_LOG = 'stdout';
+if (!process.env.NEW_RELIC_LICENSE_KEY) process.env.NEW_RELIC_ENABLED = 'false'
+process.env.NEW_RELIC_LOG = 'stdout'
 
-let koa      = require('koa');
-let compress = require('koa-compress');
-let r        = require('koa-route');
-let newrelic = require('newrelic');
-let parse    = require('co-body');
-let fs       = require('fs');
-let packages = require('./lib/packages');
-let tarballs = require('./lib/tarballs');
-let config   = require('./lib/config');
-let user     = require('./lib/user');
-let rollbar  = require('rollbar');
-let path     = require('path');
-let app      = koa();
+let koa = require('koa')
+let compress = require('koa-compress')
+let r = require('koa-route')
+let newrelic = require('newrelic')
+let parse = require('co-body')
+let fs = require('fs')
+let packages = require('./lib/packages')
+let tarballs = require('./lib/tarballs')
+let config = require('./lib/config')
+let user = require('./lib/user')
+let rollbar = require('rollbar')
+let path = require('path')
+let app = koa()
 
 if (config.rollbar) {
   rollbar.init(config.rollbar, {
-    environment:  process.env.NODE_ENV,
-    codeVersion:  process.env.HEROKU_SLUG_COMMIT,
-    host:         process.env.HEROKU_APP_NAME,
-    scrubHeaders: ['authorization'],
-  });
-  rollbar.handleUncaughtExceptions(config.rollbar);
+    environment: process.env.NODE_ENV,
+    codeVersion: process.env.HEROKU_SLUG_COMMIT,
+    host: process.env.HEROKU_APP_NAME,
+    scrubHeaders: ['authorization']
+  })
+  rollbar.handleUncaughtExceptions(config.rollbar)
 }
 
-app.name = 'elephant';
-app.port = config.port;
+app.name = 'elephant'
+app.port = config.port
 
-app.use(require('./logger'));
-app.use(compress());
+app.use(require('./logger'))
+app.use(compress())
 
 // static root page
-app.use(r.get('/', function* () {
-  newrelic.setTransactionName('');
-  this.type = 'text/html';
-  this.body = fs.createReadStream(__dirname + '/public/index.html');
-}));
+app.use(r.get('/', function * () {
+  newrelic.setTransactionName('')
+  this.type = 'text/html'
+  this.body = fs.createReadStream(path.join(__dirname, '/public/index.html'))
+}))
 
 // ping
-app.use(r.get('/-/ping', function *() {
-  newrelic.setTransactionName('-/ping');
-  this.body = {};
-}));
-
+app.use(r.get('/-/ping', function * () {
+  newrelic.setTransactionName('-/ping')
+  this.body = {}
+}))
 
 // error middleware
-app.use(function* (next) {
-  try { yield next; }
-  catch (err) {
-    newrelic.noticeError(err);
-    if (config.rollbar) rollbar.handleError(err, this.request);
-    this.status = err.status || 500;
-    this.body   = {error: err.message};
-    this.app.emit('error', err, this);
+app.use(function * (next) {
+  try { yield next } catch (err) {
+    newrelic.noticeError(err)
+    if (config.rollbar) rollbar.handleError(err, this.request)
+    this.status = err.status || 500
+    this.body = {error: err.message}
+    this.app.emit('error', err, this)
   }
-});
+})
 
 // get package metadata
-app.use(r.get('/:name', function *(name) {
-  newrelic.setTransactionName(':name');
-  let etag = this.req.headers['if-none-match'];
-  let pkg = yield packages(this.metric).get(name, etag);
+app.use(r.get('/:name', function * (name) {
+  newrelic.setTransactionName(':name')
+  let etag = this.req.headers['if-none-match']
+  let pkg = yield packages(this.metric).get(name, etag)
   if (pkg === 304) {
-    this.status = 304;
-    return;
+    this.status = 304
+    return
   }
   if (pkg === 404) {
-    this.status = 404;
-    this.body   = {error: 'no such package available'};
-    return;
+    this.status = 404
+    this.body = {error: 'no such package available'}
+    return
   }
-  let cloudfront = this.headers['user-agent'] === 'Amazon CloudFront';
-  packages(this.metric).rewriteTarballURLs(pkg, cloudfront ? config.cloudfrontHost : this.headers.host);
-  this.set('ETag', pkg.etag);
-  this.set('Cache-Control', `public, max-age=${config.cache.packageTTL}`);
-  this.body = pkg;
-}));
+  let cloudfront = this.headers['user-agent'] === 'Amazon CloudFront'
+  packages(this.metric).rewriteTarballURLs(pkg, cloudfront ? config.cloudfrontHost : this.headers.host)
+  this.set('ETag', pkg.etag)
+  this.set('Cache-Control', `public, max-age=${config.cache.packageTTL}`)
+  this.body = pkg
+}))
 
 // get package tarball with sha
-app.use(r.get('/:name/-/:filename/:sha', function *(name, filename, sha) {
-  newrelic.setTransactionName(':name/-/:filename/:sha');
-  let tarball = yield tarballs(this.metric).get(name, filename, sha);
+app.use(r.get('/:name/-/:filename/:sha', function * (name, filename, sha) {
+  newrelic.setTransactionName(':name/-/:filename/:sha')
+  let tarball = yield tarballs(this.metric).get(name, filename, sha)
   if (!tarball) {
-    this.status = 404;
-    this.body   = {error: 'no tarball found'};
-    return;
+    this.status = 404
+    this.body = {error: 'no tarball found'}
+    return
   }
-  this.set('Content-Length', tarball.size);
-  this.set('Cache-Control', `public, max-age=${config.cache.tarballTTL}`);
-  this.body = tarball;
-}));
+  this.set('Content-Length', tarball.size)
+  this.set('Cache-Control', `public, max-age=${config.cache.tarballTTL}`)
+  this.body = tarball
+}))
 
 // get package tarball with sha
-app.use(r.get('/:scope/:name/-/:filename/:sha', function *(scope, name, filename, sha) {
-  newrelic.setTransactionName(':scope/:name/-/:filename/:sha');
-  let tarball = yield tarballs(this.metric).get(`${scope}/${name}`, filename, sha);
+app.use(r.get('/:scope/:name/-/:filename/:sha', function * (scope, name, filename, sha) {
+  newrelic.setTransactionName(':scope/:name/-/:filename/:sha')
+  let tarball = yield tarballs(this.metric).get(`${scope}/${name}`, filename, sha)
   if (!tarball) {
-    this.status = 404;
-    this.body   = {error: 'no tarball found'};
-    return;
+    this.status = 404
+    this.body = {error: 'no tarball found'}
+    return
   }
-  this.set('Content-Length', tarball.size);
-  this.set('Cache-Control', `public, max-age=${config.cache.tarballTTL}`);
-  this.body = tarball;
-}));
+  this.set('Content-Length', tarball.size)
+  this.set('Cache-Control', `public, max-age=${config.cache.tarballTTL}`)
+  this.body = tarball
+}))
 
 // get package tarball without sha
-app.use(r.get('/:name/-/:filename', function *(name, filename) {
-  newrelic.setTransactionName(':name/-/:filename');
-  let ext = path.extname(filename);
-  filename = path.basename(filename, ext);
-  this.redirect(`/${name}/-/${filename}/a${ext}`);
-}));
+app.use(r.get('/:name/-/:filename', function * (name, filename) {
+  newrelic.setTransactionName(':name/-/:filename')
+  let ext = path.extname(filename)
+  filename = path.basename(filename, ext)
+  this.redirect(`/${name}/-/${filename}/a${ext}`)
+}))
 
 // login
-app.use(r.put('/-/user/:user', function *() {
-  newrelic.setTransactionName('-/user/:user');
-  let auth = yield user.authenticate(yield parse(this));
+app.use(r.put('/-/user/:user', function * () {
+  newrelic.setTransactionName('-/user/:user')
+  let auth = yield user.authenticate(yield parse(this))
   if (auth) {
-    this.status = 201;
-    this.body   = {token: auth};
+    this.status = 201
+    this.body = {token: auth}
   } else {
-    this.status = 401;
-    this.body   = {error: "invalid credentials"};
+    this.status = 401
+    this.body = {error: 'invalid credentials'}
   }
-}));
+}))
 
 // authenticate
-app.use(function* (next) {
+app.use(function * (next) {
   if (!this.headers.authorization) {
-    this.status = 401;
-    this.body   = {error: 'no credentials provided'};
-    return;
+    this.status = 401
+    this.body = {error: 'no credentials provided'}
+    return
   }
-  let token = this.headers.authorization.split(' ')[1];
-  this.username = yield user.findByToken(token);
+  let token = this.headers.authorization.split(' ')[1]
+  this.username = yield user.findByToken(token)
   if (!this.username) {
-    this.status = 401;
-    this.body   = {error: 'invalid credentials'};
-    return;
+    this.status = 401
+    this.body = {error: 'invalid credentials'}
+    return
   }
-  yield next;
-});
+  yield next
+})
 
 // whoami
-app.use(r.get('/-/whoami', function *() {
-  newrelic.setTransactionName('-/whoami');
-  this.body = {username: this.username};
-}));
+app.use(r.get('/-/whoami', function * () {
+  newrelic.setTransactionName('-/whoami')
+  this.body = {username: this.username}
+}))
 
 // npm publish
-app.use(r.put('/:name', function *() {
-  newrelic.setTransactionName(':name');
-  let pkg = yield parse(this);
+app.use(r.put('/:name', function * () {
+  newrelic.setTransactionName(':name')
+  let pkg = yield parse(this)
   try {
-    yield packages(this.metric).upload(pkg);
-    this.body = yield packages(this.metric).get(pkg.name);
+    yield packages(this.metric).upload(pkg)
+    this.body = yield packages(this.metric).get(pkg.name)
   } catch (err) {
     if (err === packages(this.metric).errors.versionExists) {
-      this.body   = {error: err.toString()};
-      this.status = 409;
+      this.body = {error: err.toString()}
+      this.status = 409
     } else {
-      throw err;
+      throw err
     }
   }
-}));
+}))
 
-module.exports = app;
+module.exports = app
 if (!module.parent) {
   app.listen(app.port, function () {
-    console.error(`${app.name} listening on port ${app.port} [${app.env}]`);
-  });
+    console.error(`${app.name} listening on port ${app.port} [${app.env}]`)
+  })
 }
--- test/user.js
+++ test/user.js
@@ -1,62 +1,64 @@
-'use strict';
+'use strict'
+/* global describe it before */
 
-let app     = require('../server');
-let request = require('supertest-as-promised').agent(app.listen());
-let user    = require('../lib/user');
-let co      = require('co');
+let app = require('../server')
+let request = require('supertest-as-promised').agent(app.listen())
+let user = require('../lib/user')
+let co = require('co')
+let expect = require('unexpected')
 
 // make sure this user is in the htpasswd file
-const testUser = {name: 'test', password: 'test'};
+const testUser = {name: 'test', password: 'test'}
 
 function bearer (token) {
   return function (request) {
-    request.set('Authorization', `Bearer ${token}`);
-  };
+    request.set('Authorization', `Bearer ${token}`)
+  }
 }
 
 describe('user', () => {
-  let token;
+  let token
 
   describe('/-/whoami (whoami)', () => {
     describe('logged in', () => {
-      before(co.wrap(function* () {
-        token = yield user.authenticate(testUser);
-      }));
+      before(co.wrap(function * () {
+        token = yield user.authenticate(testUser)
+      }))
 
       it('returns the username', () => {
         return request.get('/-/whoami')
-        .use(bearer(token))
-        .accept('json')
-        .expect(200)
-        .then(res => expect(res.body.username).to.eq('test'));
-      });
-    });
+          .use(bearer(token))
+          .accept('json')
+          .expect(200)
+          .then((res) => expect(res.body.username, 'to equal', 'test'))
+      })
+    })
     describe('anonymous', () => {
       it('returns 401', () => {
         return request.get('/-/whoami')
-        .accept('json')
-        .expect(401);
-      });
-    });
-  });
+          .accept('json')
+          .expect(401)
+      })
+    })
+  })
 
   describe('/-/user/:user (login)', () => {
     describe('valid credentials', () => {
       it('gets the token', () => {
         return request.put('/-/user/test')
-        .send({name: 'test', password: 'test'})
-        .accept('json')
-        .expect(201)
-        .then(res => expect(res.body).to.have.property('token'));
-      });
-    });
+          .send({name: 'test', password: 'test'})
+          .accept('json')
+          .expect(201)
+          .then((res) => expect(res.body, 'to have property', 'token'))
+      })
+    })
     describe('invalid credentials', () => {
       it('returns 401', () => {
         return request.put('/-/user/test')
-        .send({name: 'test', password: 'invalid'})
-        .accept('json')
-        .expect(401);
-      });
-    });
-  });
-});
+          .send({name: 'test', password: 'invalid'})
+          .accept('json')
+          .expect(401)
+      })
+    })
+  })
+})
