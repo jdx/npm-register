75190b0889644e555d240440290acc003b16e81a
Jeff Dickey <dickeyxxx@gmail.com>
2016-08-10 15:32:47 -0700

    do not default tests to use fs

--- test/init.js
+++ test/init.js
@@ -2,7 +2,4 @@
 
 const fs = require('fs-extra')
 
-process.env.NPM_REGISTER_STORAGE = 'fs'
-process.env.NPM_REGISTER_FS_DIRECTORY = 'tmp'
-
 fs.outputFileSync('./tmp/htpasswd', 'test:$2y$05$ZhGKbrjyUbSbiMUeYeRUKOXPKzs9./NIZHsycrQkUKIj1Z2VybqdK\n')
