6d6034e2b9475e478a88b7e0147e346401096b71
Jeff Dickey <dickeyxxx@gmail.com>
2016-08-10 17:17:46 -0700

    test on both s3 and fs

--- test/user.js
+++ test/user.js
@@ -6,6 +6,7 @@ const request = require('supertest-as-promised').agent(app.listen())
 const user = require('../lib/user')
 const co = require('co')
 const expect = require('unexpected')
+const config = require('../config')
 
 // make sure this user is in the htpasswd file
 const testUser = {name: 'test', password: 'test'}
@@ -16,48 +17,56 @@ function bearer (token) {
   }
 }
 
-describe('user', () => {
-  let token
-
-  describe('/-/whoami (whoami)', () => {
-    describe('logged in', () => {
-      before(co.wrap(function * () {
-        token = yield user.authenticate(testUser)
-      }))
-
-      it('returns the username', () => {
-        return request.get('/-/whoami')
-          .use(bearer(token))
-          .accept('json')
-          .expect(200)
-          .then((res) => expect(res.body.username, 'to equal', 'test'))
-      })
+['fs', 's3'].forEach(storage => {
+  describe(storage, () => {
+    before(() => {
+      let Storage = require('../lib/storage/' + storage)
+      config.storage = new Storage()
     })
-    describe('anonymous', () => {
-      it('returns 401', () => {
-        return request.get('/-/whoami')
-          .accept('json')
-          .expect(401)
-      })
-    })
-  })
+    describe('user', () => {
+      let token
 
-  describe('/-/user/:user (login)', () => {
-    describe('valid credentials', () => {
-      it('gets the token', () => {
-        return request.put('/-/user/test')
-          .send({name: 'test', password: 'test'})
-          .accept('json')
-          .expect(201)
-          .then((res) => expect(res.body, 'to have property', 'token'))
+      describe('/-/whoami (whoami)', () => {
+        describe('logged in', () => {
+          before(co.wrap(function * () {
+            token = yield user.authenticate(testUser)
+          }))
+
+          it('returns the username', () => {
+            return request.get('/-/whoami')
+              .use(bearer(token))
+              .accept('json')
+              .expect(200)
+              .then((res) => expect(res.body.username, 'to equal', 'test'))
+          })
+        })
+        describe('anonymous', () => {
+          it('returns 401', () => {
+            return request.get('/-/whoami')
+              .accept('json')
+              .expect(401)
+          })
+        })
       })
-    })
-    describe('invalid credentials', () => {
-      it('returns 401', () => {
-        return request.put('/-/user/test')
-          .send({name: 'test', password: 'invalid'})
-          .accept('json')
-          .expect(401)
+
+      describe('/-/user/:user (login)', () => {
+        describe('valid credentials', () => {
+          it('gets the token', () => {
+            return request.put('/-/user/test')
+              .send({name: 'test', password: 'test'})
+              .accept('json')
+              .expect(201)
+              .then((res) => expect(res.body, 'to have property', 'token'))
+          })
+        })
+        describe('invalid credentials', () => {
+          it('returns 401', () => {
+            return request.put('/-/user/test')
+              .send({name: 'test', password: 'invalid'})
+              .accept('json')
+              .expect(401)
+          })
+        })
       })
     })
   })
