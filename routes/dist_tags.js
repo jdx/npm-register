f0611c37d332a7a3025d79f8a83a05150b0f83ab
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2016-12-07 15:12:34 +0000

    check local packages before npmjs.org packages
    also throws 404s when packages are not found

--- routes/dist_tags.js
+++ routes/dist_tags.js
@@ -10,20 +10,19 @@ let getPackage = name => config.storage.getJSON(`packages/${name}`)
 
 r.get('/-/package/:name/dist-tags', function * () {
   let {name} = this.params
-  let pkg = yield npm.get(name)
-  if (pkg !== 404) this.body = pkg['dist-tags']
-  else {
-    let pkg = yield getPackage(name)
-    this.body = pkg['dist-tags']
+  let pkg = yield getPackage(name)
+  if (!pkg) {
+    pkg = yield npm.get(name)
+    if (pkg === 404) this.throw(404)
   }
+  this.body = pkg['dist-tags']
 })
 
 r.put('/-/package/:name/dist-tags/:tag', middleware.auth, function * () {
   let {name, tag} = this.params
   let version = JSON.parse(yield parse.text(this))
-  let pkg = yield npm.get(name)
-  if (pkg !== 404) this.throw(400, `Cannot set dist-tags, ${name} is hosted on ${config.uplink.host}`)
-  pkg = yield getPackage(name)
+  let pkg = yield getPackage(name)
+  if (!pkg) this.throw(404)
   pkg['dist-tags'] = Object.assign(pkg['dist-tags'], {[tag]: version})
   yield config.storage.put(`packages/${name}`, pkg)
   this.body = {}
@@ -31,9 +30,8 @@ r.put('/-/package/:name/dist-tags/:tag', middleware.auth, function * () {
 
 r.delete('/-/package/:name/dist-tags/:tag', middleware.auth, function * () {
   let {name, tag} = this.params
-  let pkg = yield npm.get(name)
-  if (pkg !== 404) this.throw(400, `Cannot delete dist-tags, ${name} is hosted on ${config.uplink.host}`)
-  pkg = yield getPackage(name)
+  let pkg = yield getPackage(name)
+  if (!pkg) this.throw(404)
   delete pkg['dist-tags'][tag]
   yield config.storage.put(`packages/${name}`, pkg)
   this.body = {}
