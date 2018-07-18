05869d30d0b3f2ffbb05a6572c885199af87d1ac
Dan Gautsch <dgautsch@gmail.com>
2018-07-17 21:10:10 -0400

    Fixing indentations to match standard style

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -4,9 +4,9 @@ const webpack = require('webpack')
 module.exports = {
   plugins: [
     new webpack.DefinePlugin({
-  	  'process.env': {
-  	    NODE_ENV: JSON.stringify('production')
-  	  }
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
     }),
     new UglifyJSPlugin()
   ]
