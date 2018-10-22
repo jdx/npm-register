6201050e9ae8d304a3b32d874987309ec1f3e09b
Dan Gautsch <dan@gautsch.codes>
2018-10-21 21:57:52 -0400

    Testing undefined values for package properties

--- test/storage.js
+++ test/storage.js
@@ -3,35 +3,51 @@ const expect = require('chai').expect
 
 describe('storage', () => {
   describe('base storage methods', () => {
-    let mockJson = {}
+    let mockJson = {
+      name: 'foo',
+      'dist-tags': {
+        latest: 1
+      },
+      versions: {
+        1: {
+          author: 'bar',
+          dist: 'baz'
+        }
+      }
+    }
     let base
     let packageDetails
+
     beforeEach(() => {
       base = new Base()
     })
+
+    afterEach(() => {
+      mockJson.name = 'foo'
+      mockJson['dist-tags'].latest = 1
+    })
+
     it('should escape if name property is undefined or empty string', () => {
       mockJson.name = ''
+      expect(base).to.be.an.instanceof(Base)
       expect(base.createPackageDetails(mockJson)).to.be.an('undefined')
     })
+
     it('should return a package object', () => {
-      mockJson = {
-        name: 'foo',
-        'dist-tags': {
-          latest: 1
-        },
-        versions: {
-          1: {
-            author: 'bar',
-            dist: 'baz'
-          }
-        }
-      }
       packageDetails = base.createPackageDetails(mockJson)
+      expect(base).to.be.an.instanceof(Base)
       expect(packageDetails).to.be.an('object')
       expect(packageDetails.name).to.equal('foo')
       expect(packageDetails.currentVersion).to.equal(1)
       expect(packageDetails.author).to.equal('bar')
       expect(packageDetails.tarball).to.equal('baz')
+    })
+
+    it('should return an empty string if latest is null', () => {
+      delete mockJson['dist-tags'].latest
+      packageDetails = base.createPackageDetails(mockJson)
+      expect(base).to.be.an.instanceof(Base)
+      expect(packageDetails.currentVersion).to.equal('')
     })
   })
 })
